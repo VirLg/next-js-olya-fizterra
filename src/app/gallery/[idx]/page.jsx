86976907e60837f371a597357d59525a494cfdee
VirLg <19virlg@gmail.com>
2024-02-25 15:18:11 +0200

    fix

--- src/app/gallery/[idx]/page.jsx
+++ src/app/gallery/[idx]/page.jsx
@@ -3,6 +3,8 @@ import arr from '../galleryData';
 const SingleItem = ({ idx }) => {
   console.log('idx', idx);
 
-  return <div className="container bg-lime-100 h-[80px]">Gallery item</div>;
+  return (
+    <div className="container bg-lime-100 h-[80px]">Gallery item {idx}</div>
+  );
 };
 export default SingleItem;
