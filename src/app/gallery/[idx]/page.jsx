1d49b73e44f50a374b009e6f886686370739fd63
VirLg <19virlg@gmail.com>
2024-02-25 15:16:03 +0200

    fix export

--- src/app/gallery/[idx]/page.jsx
+++ src/app/gallery/[idx]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import arr from '../galleryData';
-export default function SingleItem({ idx }) {
+export const SingleItem = ({ idx }) => {
   console.log('idx', idx);
 
   return <div className="container bg-lime-100 h-[80px]">Gallery item</div>;
-}
+};
